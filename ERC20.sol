d70368084300ae635f2ce9a8dc9485870d1f94cd
HDEROCHE <hugo.deroche@gmail.com>
2021-03-12 10:48:58 +0100

    Proto

--- ERC20.sol
+++ ERC20.sol
@@ -1,1 +1,8 @@
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.0;
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol'
+
+contract MyToken is ERC20{
+    constructor() {
+        
+    }
+}

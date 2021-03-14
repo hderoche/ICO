af1a8ec0fccec8a01fdf8a915605f5a5b743e27d
HDEROCHE <hugo.deroche@gmail.com>
2021-03-14 18:49:55 +0100

    Testnet deployment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 const Migrations = artifacts.require("Migrations");
+const data = require('../secret.json');
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(Migrations, {from: data.address});
 };
--- migrations/2_myToken.js
+++ migrations/2_myToken.js
@@ -1,5 +1,5 @@
 const MyToken = artifacts.require("MyToken");
-
-module.exports = function (deployer) {
-  deployer.deploy(MyToken, 10, 1000000);
+const data = require('../secret.json');
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(MyToken, 10, 1000000, {from: data.address});
 };
